439028c74a5867a8cc18e67ba4d2a54b53747b9e
ternsip <ternsip@gmail.com>
2019-06-02 19:17:12 +0300

    Adjust some todos;

--- src/main/java/com/ternsip/glade/Glade.java
+++ src/main/java/com/ternsip/glade/Glade.java
@@ -5,10 +5,12 @@ import com.ternsip.glade.graphics.renderer.base.MasterRenderer;
 import com.ternsip.glade.universe.Universe;
 
 // TODO TURN ALL Vectors and Matrixes and Quaternions to constant interface (quatenrionfc/matrixfc)]
-// TODO FONTS
 // TODO SHADOWING ?
 // TODO PHYSICAL COLLISIONS
 // TODO read about MemoryStack for optimising buffer allocation
+// TODO LookAt bug (collinear)
+// TODO Animated Textures
+// TODO Author rights
 public class Glade {
 
     public static final DisplayManager DISPLAY_MANAGER = new DisplayManager();
