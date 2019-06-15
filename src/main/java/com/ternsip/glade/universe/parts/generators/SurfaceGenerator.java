d425e86beb0560d86296cc36b62556a25d34c390
ternsip <ternsip@gmail.com>
2019-06-15 19:14:17 +0300

    Change generation + grass texture;

--- src/main/java/com/ternsip/glade/universe/parts/generators/SurfaceGenerator.java
+++ src/main/java/com/ternsip/glade/universe/parts/generators/SurfaceGenerator.java
@@ -11,9 +11,6 @@ import org.joml.Vector3ic;
 public class SurfaceGenerator implements ChunkGenerator {
 
     private final int height = 60;
-    private final float compress = 0.01f;
-    private final int deviation = 5;
-    private final int terraces = 15;
 
     @Override
     public int getPriority() {
@@ -22,28 +19,32 @@ public class SurfaceGenerator implements ChunkGenerator {
 
     @Override
     public void populate(Chunk chunk) {
-        int[][] heightMap = generateHeightMap(chunk);
+        int[][] heightMapStone = generateHeightMap(chunk, 11, 5, 50, 0.01f);
+        int[][] heightMapDirt = generateHeightMap(chunk, 60, 5, 20, 0.01f);
         chunk.forEach((Vector3ic pos, Block block, int light) -> {
             Vector3ic wPos = chunk.toWorldPos(pos);
-            int curHeight = heightMap[pos.x()][pos.z()];
-            if (wPos.y() <= curHeight) {
+            int stoneHeight = height + heightMapStone[pos.x()][pos.z()];
+            int dirtHeight = stoneHeight + heightMapDirt[pos.x()][pos.z()];
+            if (wPos.y() < stoneHeight) {
                 chunk.setBlock(pos, Block.STONE);
+            } else if (wPos.y() <= dirtHeight) {
+                chunk.setBlock(pos, wPos.y() == dirtHeight ? Block.LAWN : Block.DIRT);
             }
         });
     }
 
-    private int[][] generateHeightMap(Chunk chunk) {
+    private int[][] generateHeightMap(Chunk chunk, float seed, long terraces, float deviation, float compress) {
         int[][] heightMap = new int[Chunk.SIZE][Chunk.SIZE];
         for (int x = 0; x < Chunk.SIZE; ++x) {
             for (int z = 0; z < Chunk.SIZE; ++z) {
                 Vector3ic wPos = chunk.toWorldPos(new Vector3i(x, 0, z));
-                float noiseX = wPos.x() * getCompress();
-                float noiseZ = wPos.z() * getCompress();
-                double noise1 = SimplexNoise.noise(noiseX, noiseZ) * getDeviation();
-                double noise2 = SimplexNoise.noise(noiseZ, noiseX) * getDeviation();
-                long diff = Math.round(Math.pow(noise1, Math.E) * getTerraces()) / getTerraces();
-                long diff2 = -Math.round(Math.pow(noise2, Math.E) * getTerraces()) / getTerraces();
-                heightMap[x][z] = (int) (getHeight() + diff + diff2);
+                float noiseX = wPos.x() * compress;
+                float noiseZ = wPos.z() * compress;
+                double noise1 = SimplexNoise.noise(noiseX, noiseZ, seed);
+                double noise2 = SimplexNoise.noise(noiseZ, noiseX, seed);
+                long diff = Math.round(Math.pow(noise1, Math.E) * terraces * deviation) / terraces;
+                long diff2 = -Math.round(Math.pow(noise2, Math.E) * terraces * deviation) / terraces;
+                heightMap[x][z] = (int) (diff + diff2);
             }
         }
         return heightMap;
