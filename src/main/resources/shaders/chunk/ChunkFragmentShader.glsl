8ae093655f4a7fc60df1f888fb3169c04ee0e086
ternsip <ternsip@gmail.com>
2019-10-04 15:52:10 +0300

    Fix bug in prev commit;

--- src/main/resources/shaders/chunk/ChunkFragmentShader.glsl
+++ src/main/resources/shaders/chunk/ChunkFragmentShader.glsl
@@ -33,7 +33,7 @@ void main(void){
     if (isBlockOfType(BLOCK_TYPE_WATER)) {
         vec2 cPos = -1.0 + 2.0 * passWorldPos.xz;
         float cLength = length(cPos);
-        vec2 uv = passWorldPos.xz / cc + (cPos / cLength) * cos(cLength * 12.0 - M_PI * 2 * time * 1) * 0.03;
+        vec2 uv = passWorldPos.xz + (cPos / cLength) * cos(cLength * 12.0 - M_PI * 2 * time * 1) * 0.03;
         uv = abs(vec2(mod(uv.x, 1.0), mod(uv.y, 1.0)));
         vec3 resultColor = passAmbient * texture(samplers[roundFloat(passAtlasNumber)], vec3(uv * passAtlasMaxUV, roundFloat(passAtlasLayer))).xyz;
         out_colour = vec4(passAmbient * mix(fogColor, resultColor, visibility), 0.8);
