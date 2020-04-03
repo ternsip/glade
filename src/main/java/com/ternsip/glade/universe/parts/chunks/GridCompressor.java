49e998543e5ff4973ea101637f5084b21d97dc93
ternsip <ternsip@gmail.com>
2020-04-03 17:35:09 +0300

    Document octree; Important step for future investigation;

--- src/main/java/com/ternsip/glade/universe/parts/chunks/GridCompressor.java
+++ src/main/java/com/ternsip/glade/universe/parts/chunks/GridCompressor.java
@@ -15,6 +15,11 @@ import java.util.*;
 
 import static com.ternsip.glade.universe.parts.chunks.BlocksServerRepository.*;
 
+/**
+ * Compressor of of 3d space of integer cells (voxels/blocks/cubes)
+ * Uses octree to infuse similar nodes and to not duplicate memory when similar pattern occurs
+ * The more diversity 3d space have - the less compression level is
+ */
 public class GridCompressor implements Serializable {
 
     private static final int SIZE = Maths.max(SIZE_X, SIZE_Y, SIZE_Z);
@@ -22,11 +27,19 @@ public class GridCompressor implements Serializable {
     private static final int DEPTH = Maths.log2(NODES);
     private static final int CHUNK_DEPTH = Maths.log2(NODES / Chunk.NODES);
 
+    // Special buffers to decrease memory bandwidth
     private Hash[] hashesBuffer = new Hash[Chunk.NODES * Chunk.NODES * Chunk.NODES];
     private int[] indexPathBuffer = new int[CHUNK_DEPTH];
     private Node[] nodePathBuffer = new Node[CHUNK_DEPTH];
+
+    // This map is used to cache chunks by chunk-coordinate
     private HashMap<Vector3i, Chunk> posToChunk = new HashMap<>();
+
+    // Octree general structure, holding mapping: hash of some node -> to that node
+    // Remember that leaf nodes are just hashes that keeps real data, not some kind of other classes
     private HashMap<Hash, Node> hashToNode = new HashMap<>();
+
+    // The root of octree
     private Hash root;
 
     static {
@@ -39,6 +52,7 @@ public class GridCompressor implements Serializable {
     }
 
     public GridCompressor() {
+        // Initially fill new tree with zero values, it takes really small number of nodes due to repetitions
         Hash hash = new Hash();
         for (int i = 0; i < DEPTH; ++i) {
             Node node = new Node(hash);
@@ -91,6 +105,7 @@ public class GridCompressor implements Serializable {
         // TODO clean after saving periodically, otherwise you can get too many dead nodes
     }
 
+    // Cleaning obsoleted nodes from map, using simple dfs to mark all used nodes starting from root
     public synchronized void cleanTree() {
         Set<Hash> used = new HashSet<>();
         Stack<Hash> stack = new Stack<>();
@@ -114,6 +129,8 @@ public class GridCompressor implements Serializable {
         if (chunk == null) {
             chunk = new Chunk();
             hashesBuffer[0] = traceChunk(cx, cy, cz);
+            // Filling hashes buffer plain array with chunk's leafs
+            // Iterating from top to bottom levels of tree, the reverse process is fully described in save-stage
             for (int step = hashesBuffer.length; step >= 8; step /= 8) {
                 for (int ptr = 0; ptr < hashesBuffer.length; ptr += step) {
                     Node node = hashToNode.get(hashesBuffer[ptr]);
@@ -123,6 +140,7 @@ public class GridCompressor implements Serializable {
                     }
                 }
             }
+            // Filling chunk with values taken from hashes buffer in natural order described in save-stage
             for (int nx = 0, hashIndex = 0; nx < Chunk.NODES; ++nx) {
                 for (int ny = 0; ny < Chunk.NODES; ++ny) {
                     for (int nz = 0; nz < Chunk.NODES; ++nz, ++hashIndex) {
@@ -136,6 +154,7 @@ public class GridCompressor implements Serializable {
                     }
                 }
             }
+            // Buffering chunk using map
             posToChunk.put(pos, chunk);
         }
         return chunk;
@@ -149,6 +168,11 @@ public class GridCompressor implements Serializable {
         if (chunk == null) {
             throw new IllegalArgumentException("Chunk not found");
         }
+        // Keeps data in next order:  Sum_i [i in 0..n] of 8^i * (4xi + 2yi + 1zi) = index
+        // Where xi or yi or zi is binary digit (1 | 0) representation of final coordinate
+        // In other words index holds n triplets of coordinates in order: xi (highest), yi (middle), zi (lowest)
+        // The final coordinate (x or y or z) should be composed by binary concatenation of all n components
+        // All that is just natural order to pack all 3d values into flat array
         for (int nx = 0, hashIndex = 0; nx < Chunk.NODES; ++nx) {
             for (int ny = 0; ny < Chunk.NODES; ++ny) {
                 for (int nz = 0; nz < Chunk.NODES; ++nz, ++hashIndex) {
@@ -163,6 +187,10 @@ public class GridCompressor implements Serializable {
                 }
             }
         }
+        // Filling hashes buffer by next algorithm: compressing every oct-children into one parent
+        // Starting from smallest step because initially it is just flattened tree leafs into plain array
+        // The hole process can be described as iterating through levels of tree starting from bottom to top
+        // Remember that this is just flattened chunk's nodes, not whole tree
         for (int step = 8; step <= hashesBuffer.length; step *= 8) {
             for (int ptr = 0; ptr < hashesBuffer.length; ptr += step) {
                 Node node = new Node();
@@ -175,15 +203,23 @@ public class GridCompressor implements Serializable {
             }
         }
         traceChunk(cx, cy, cz);
+        // Now Buffer holds only one element (index - zero) - the top of chunk's node
+        // So we need to move further uphill on the tree until we reach root level
+        // For each node we pass through - we modifying one child and recording new hash sum for the node
         Hash updatedHash = hashesBuffer[0];
         for (int depth = CHUNK_DEPTH - 1; depth >= 0; --depth) {
             Node updatedNode = nodePathBuffer[depth].cloneWithChangedChild(indexPathBuffer[depth], updatedHash);
             updatedHash = updatedNode.combine();
             hashToNode.put(updatedHash, updatedNode);
         }
+        // After all the process we just appointing new root
+        // But old root and some data still remain in the node-map, hence we need to clean it in separate routine
         root = updatedHash;
     }
 
+    // Recording path from (top) root node to (somewhere in middle) chunk's node
+    // Keeping nodes and indices of the children we falling in
+    // As the result we return chunk's top node
     private Hash traceChunk(int cx, int cy, int cz) {
         Hash pointer = root;
         for (int depth = 0, halfChunks = NODES / (2 * Chunk.NODES); depth < CHUNK_DEPTH; ++depth, halfChunks /= 2) {
@@ -209,6 +245,7 @@ public class GridCompressor implements Serializable {
     }
 
     private synchronized void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
+        // TODO infuse with class default definition, get rid of boilerplate
         hashesBuffer = new Hash[Chunk.NODES * Chunk.NODES * Chunk.NODES];
         indexPathBuffer = new int[CHUNK_DEPTH];
         nodePathBuffer = new Node[CHUNK_DEPTH];
@@ -254,6 +291,11 @@ public class GridCompressor implements Serializable {
         }
     }
 
+    /**
+     * This hash used for both - store real values for leaf-nodes of tree and store tree node's hash
+     * Since leaf nodes are being used to keep real values, we can pack one octree node in it - 8 values
+     * That way every leaf in octree is stored like hash
+     */
     @NoArgsConstructor
     @EqualsAndHashCode
     private static class Hash {
@@ -261,6 +303,7 @@ public class GridCompressor implements Serializable {
         private static final int LENGTH = 8;
         private final int[] values = new int[LENGTH];
 
+        // Constructing class from byte array (byte array -> int array)
         private Hash(byte[] bytes) {
             for (int i = 0, j = 0; i < LENGTH; ++i, j += 4) {
                 values[i] |= ((0xFF & bytes[j]) << 24) | ((0xFF & bytes[j + 1]) << 16) | ((0xFF & bytes[j + 2]) << 8) | (0xFF & bytes[j + 3]);
@@ -278,6 +321,7 @@ public class GridCompressor implements Serializable {
             Arrays.fill(children, hash);
         }
 
+        // Generate hash regarding children values
         private Hash combine() {
             ByteBuffer byteBuffer = ByteBuffer.allocate(Hash.LENGTH * children.length * Integer.BYTES);
             for (int i = 0; i < Hash.LENGTH; ++i) {
@@ -290,9 +334,7 @@ public class GridCompressor implements Serializable {
 
         private Node cloneWithChangedChild(int index, Hash hash) {
             Node updatedNode = new Node();
-            for (int i = 0; i < children.length; ++i) {
-                updatedNode.children[i] = children[i];
-            }
+            System.arraycopy(children, 0, updatedNode.children, 0, children.length);
             updatedNode.children[index] = hash;
             return updatedNode;
         }
